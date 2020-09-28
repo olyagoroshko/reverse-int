2399b69a1334ed75145061cd93873833701fdad1
olyagoroshko <olga.og.goroshko@gmail.com>
2020-09-28 18:18:52 +0300

    feat: add reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+  if( n < 0 ){
+      n= (n * (-1)).toString().split('').reverse().join("");
+      return n;
+  } else {
+    n = n.toString().split('').reverse().join("");
+      return n;
+  }
 }
